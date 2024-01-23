6c3fd5f3ec1ae51c3050d717750d20fb57141063
SphepheloN <SphepheloN@ZAZN-BET-0B725B>
2024-01-23 11:26:54 +0200

    Add Connection String and Register DbContext for Dependency Injections 1. Connection String => appsettings.Development.json 2. Registering DbContext => Program.cs

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Register DbContext for dependency injections
+builder.Services.AddDbContext<AppDbContext>(
+    opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
 
