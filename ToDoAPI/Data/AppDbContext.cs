f9ccbd324ec3a2de593f889e6d74fe8122a05133
SphepheloN <SphepheloN@ZAZN-BET-0B725B>
2024-01-24 10:32:42 +0200

    Migrations Open project in Terminal Run the following commands 1. dotnet ef migrations add initialmigration

--- ToDoAPI/Data/AppDbContext.cs
+++ ToDoAPI/Data/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace ToDoAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<DbContext> options) :base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
         {
             
         }
